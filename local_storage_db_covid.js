694da401de97658548440c7189c434e4a6b51782
pierront <maxime.pierront@etu.u-pec.fr>
2020-06-05 01:07:31 +0200

    Chart so Beatiful

--- local_storage_db_covid.js
+++ local_storage_db_covid.js
@@ -112,6 +112,19 @@ printAll = () => {
     console.log(resQuery);
 }
 
+selectCountriesCode = () => {
+    // Initialise. If the database doesn't exist, it is created
+    var covid = new localStorageDB("covid", localStorage);
+
+    let resQuery = covid.queryAll("Countries");
+    var tabCodeCountries = [];
+
+    resQuery.forEach(element => {
+        console.log(element.CountryCode);
+        tabCodeCountries.push(element.CountryCode);
+    });
+    return tabCodeCountries;
+}
 
 resetBase = () => {
     // Initialise. If the database doesn't exist, it is created
